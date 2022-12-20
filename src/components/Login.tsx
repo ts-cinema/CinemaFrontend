8045034c478ec1da28ee8a273b92da5f4842ef28
mdokic1 <mdokic1@etf.unsa.ba>
2022-12-20 03:08:55 +0100

    Added login screen

---
+++ src/components/Login.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { TextField, Button } from '@mui/material';
+import { FiLogIn } from 'react-icons/fi';
+
+import './Login.css';
+
+const Login = () => {
+
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+    });
+
+    const onChangeHandler = (e: any) => {
+        e.preventDefault();
+        setUser({ ...user, [e.target.name]: e.target.value });
+    };
+    
+    return (
+        <div>
+            <div className="login-container">
+                <div className="login-form">
+                    <p className="login-title">Sign in</p>
+                    <p className="login-text">Sign in to access your account</p>
+                    <TextField
+                        className="input-field"
+                        id="outlined-basic"
+                        label="Email"
+                        variant="outlined"
+                        name="email"
+                        value={user.email}
+                        onChange={onChangeHandler}
+                    />
+                    <TextField
+                        className="input-field"
+                        id="outlined-password-input"
+                        label="Password"
+                        type="password"
+                        name="password"
+                        value={user.password}
+                        onChange={onChangeHandler}
+                    />
+                    <Button
+                        className="sign-in-button"
+                        variant="contained"
+                        startIcon={<FiLogIn />}
+                    >
+                        Sign in
+                    </Button>
+                </div>
+                <div className="login-background">
+                    <img className="background-image" alt="Background image" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import { ToastContainer } from 'react-toastify';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+const Login = React.lazy(() => import('./components/Login'));
+
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <BrowserRouter>
+                <React.Suspense>
+                    <ToastContainer />
+                    <Routes>
+                        <Route path={'/login'} element={ <Login />} />
+                    </Routes>
+                </React.Suspense>
+            </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
